8830e6223536cea2fcbcd95e63cb9e2eabf723a6
Julian von Steht <125369908+Julian-von-Steht@users.noreply.github.com>
2024-10-23 09:27:57 +0000

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_D8ds_v5"
+    vm_size    = "Standard_D2s_v5"
     node_count = var.node_count
   }
   /*linux_profile {
