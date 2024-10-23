518c1aa0c61e3011897721665670b6db91b279af
Julian von Steht <125369908+Julian-von-Steht@users.noreply.github.com>
2024-10-23 09:04:41 +0000

    Update main.tf
    ssh commented

--- AKS/main.tf
+++ AKS/main.tf
@@ -35,13 +35,13 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     vm_size    = "Standard_D2s_v3"
     node_count = var.node_count
   }
-  linux_profile {
+  /*linux_profile {
     admin_username = var.username
 
-    /*ssh_key {
+    ssh_key {
       key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-    }*/
-  }
+    }
+  }*/
   network_profile {
     network_plugin    = "kubenet"
     load_balancer_sku = "standard"
